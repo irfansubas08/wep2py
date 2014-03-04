2842be04bd77dc5e3b34fcfa774fc54f1f2c6215
irfan08 <irfansubas08@gmail.com>
2014-03-04 03:12:17 +0200

    Dil dosyalarını günceledim

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$aafba05d1c41f01a$22cd378c6ed4d8054ff6d8bb683535f7b4cf0b1b"
+password="pbkdf2(1000,20,sha512)$86c993ccf3808f71$d5e299fc57bbb2e6dd9ab26c03fa1c6ccfa4bddf"
